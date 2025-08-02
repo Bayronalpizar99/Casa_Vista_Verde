129d218dbfaf092f8c55e06006abfd4e87434bc2
Bayron <baalpizar@estudiantec.cr>
2025-08-02 14:59:46 -0600

    new update

--- src/types/swiper.d.ts
+++ src/types/swiper.d.ts
@@ -1,8 +1,12 @@
-
 declare module 'swiper/css';
 declare module 'swiper/css/effect-fade';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/bundle';
+declare module 'swiper/swiper-bundle.css';
 declare module 'swiper/modules';
 declare module 'swiper/react';
+
 declare module 'swiper' {
   export interface Swiper {
     slidePrev(): void;
